2c250b214cd777be2d9bda12647254115f4915e1
Shubair-zi <shubair.azizi@dcmail.ca>
2026-04-11 20:43:38 -0400

    JS-2

--- assign4/part2/main.js
+++ assign4/part2/main.js
@@ -14,3 +14,21 @@ const images = [
 
 const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
 
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
