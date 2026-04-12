182419beb7297996d39d5fa1d36a55e41efe4bfe
Shubair-zi <shubair.azizi@dcmail.ca>
2026-04-11 22:04:28 -0400

    add shape class and extend ball class

--- assign4/part4/main.js
+++ assign4/part4/main.js
@@ -12,14 +12,21 @@ function randomRGB() {
   return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -30,25 +37,17 @@ class Ball {
   }
 
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -this.velX;
-    }
-    if (this.x - this.size <= 0) {
-      this.velX = -this.velX;
-    }
-    if (this.y + this.size >= height) {
-      this.velY = -this.velY;
-    }
-    if (this.y - this.size <= 0) {
-      this.velY = -this.velY;
-    }
+    if (this.x + this.size >= width) { this.velX = -this.velX; }
+    if (this.x - this.size <= 0) { this.velX = -this.velX; }
+    if (this.y + this.size >= height) { this.velY = -this.velY; }
+    if (this.y - this.size <= 0) { this.velY = -this.velY; }
     this.x += this.velX;
     this.y += this.velY;
   }
 
   collisionDetect() {
     for (const ball of balls) {
-      if (this !== ball) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
