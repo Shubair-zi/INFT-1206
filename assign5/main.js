e34644e8461ffd6c41ee5f72cc073423760d5a65
Shubair-zi <shubair.azizi@dcmail.ca>
2026-04-17 23:52:18 -0400

    commit 2

--- assign5/main.js
+++ assign5/main.js
@@ -1,3 +1,10 @@
+/*
+Name: Shubair Azizi
+File: main.js
+Date: 17 April 2026
+JavaScript to show and hide the comments section.
+*/
+
 var showHide = document.querySelector('.show-hide');
 var commentWrapper = document.querySelector('.comment-wrapper');
 
